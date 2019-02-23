f5b29c2bbb47eb60478e3c7c36326380e6824601
Jokeren <robinho364@gmail.com>
2019-02-23 00:02:06 -0600

    Build all packages in one command

---
+++ build.sh
@@ -0,0 +1,25 @@
+PWD=`pwd`
+rm -rf install
+mkdir install
+
+# glog
+cd extern/glog
+./autogen.sh
+./configure --prefix=${PWD}/../../install/glog --enable-shared
+make -j4
+make install
+
+# gflags
+cd ../gflags
+git checkout v2.0
+./configure --prefix=${PWD}/../../install/gflags --enable-shared
+make -j4
+make install
+
+cd ../..
+
+# gBolt
+rm -rf build
+mkdir build && cd build
+cmake ..
+make -j4
