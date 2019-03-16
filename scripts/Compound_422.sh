870ab5e5ebc3ec4a948d9b52230417bee8658703
Jokeren <robinho364@gmail.com>
2019-03-15 20:27:06 -0500

    Update travis

--- scripts/Compound_422.sh
+++ scripts/Compound_422.sh
@@ -4,7 +4,7 @@ SUPPORT=(0.3 0.2 0.1 0.08)
 ANSWER=(120 932 15965 28551)
 
 
-for((i=0;i<${SUPPORT[@]};++i))
+for((i=0;i<${#SUPPORT[@]};++i))
 do
   ./build/gbolt -s ${SUPPORT[$i]} -i extern/data/Compound_422 -o /tmp/gbolt_tmp
   RES=`cat /tmp/gbolt_tmp | wc -l`
